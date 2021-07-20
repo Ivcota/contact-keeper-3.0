feaa5c9beaac8b77b74de44c27fba3ee9e77f637
Ivcota <ivcotad@gmail.com>
2021-07-20 14:38:18 -0700

    Validate information passed in the body to route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
 // @route    POST api/users
 // @desc    Registers User
 // @access    Public
-router.post("/", (req, res) => {
-  res.send("Registers User");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    res.send("Working");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
