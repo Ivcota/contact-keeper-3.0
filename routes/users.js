d48882e866ae0de55e303ed7fb0a377b1273f1c3
Ivcota <ivcotad@gmail.com>
2021-07-20 15:06:24 -0700

    user creation and password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const { user } = require("../prisma/db");
 
 // @route    POST api/users
 // @desc    Registers User
@@ -15,15 +17,52 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
       });
     }
 
-    res.send("Working");
+    const { name, email, password } = req.body;
+
+    try {
+      let aUser = await user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      console.log(aUser);
+
+      if (aUser) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      // Gen Salt Security
+      const salt = await bcrypt.genSalt(10);
+
+      console.log(salt);
+      // Hash Password
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const newUser = await user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      res.status(200).json({
+        success: true,
+        data: newUser,
+      });
+    } catch (err) {
+      console.error(err.message);
+    }
   }
 );
 
